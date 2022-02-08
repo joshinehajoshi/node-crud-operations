6e018c300d0dbe188150ab1df88111048fdfeff8
Neha Joshi <nehajoshigkp@gmail.com>
2022-02-08 13:09:50 +0530

    get and post

--- app.js
+++ app.js
@@ -4,4 +4,19 @@ const url = 'mongodb://localhost/AlienDBex'
 
 const app = express()
 
+
 mongoose.connect(url, {useNewUrlParser:true})
+const con = mongoose.connection
+
+con.on('open', () => {
+    console.log("connected...")
+})
+
+app.use(express.json())
+
+const alienRouter = require('./routers/aliens')
+app.use('/aliens', alienRouter)
+
+app.listen(5000,() => {
+    console.log('Server started.')
+})
