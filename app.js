013a4696255b1055c229f1fa65ad7af34b7808d8
Neha Joshi <nehajoshigkp@gmail.com>
2022-02-08 12:14:32 +0530

    connected to db

--- app.js
+++ app.js
@@ -1,5 +1,7 @@
 const express = require('express')
 const mongoose = require('mongoose')
+const url = 'mongodb://localhost/AlienDBex'
 
 const app = express()
 
+mongoose.connect(url, {useNewUrlParser:true})
